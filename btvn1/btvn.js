9f8e3f2ff5c39b459070f8d7a8169dd17a49a271
Github name~baoduymindxgit config --global user.name Github <nguyenhoangbaoduy7a2@hoctapkethop.com>
2023-10-27 20:48:22 +0700

    bài mới

---
+++ btvn1/btvn.js
@@ -0,0 +1,20 @@
+let mang = ["grape ", 15, "orange", 18];
+console.log("i have 15 grapes,18 oranges", mang);
+
+
+
+
+
+var Dog = {
+    "Name ": "Ngáo",
+    "legs": " 04",
+    "friends": "alsaka,bitpull"
+
+};
+console.log
+Dog.Name = "Husky"
+Dog.color = "brown"
+delete Dog.friends
+console.log(Dog);
+
+
