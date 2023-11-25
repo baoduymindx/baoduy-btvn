99379bbee128e960e8c820a3ca471c1bb6afbdaf
Github name~baoduymindxgit config --global user.name Github <nguyenhoangbaoduy7a2@hoctapkethop.com>
2023-11-25 20:48:40 +0700

    bai moi

---
+++ btvn buoi 4/data.js
@@ -0,0 +1,26 @@
+const data = [
+  {
+    id: 1,
+    name: "Addidas white",
+    price: 349,
+    img: "image1.jpg",
+  },
+  {
+    id: 2,
+    name: "nike Red",
+    price: 299,
+    img: "image2.jpg",
+  },
+  {
+    id: 3,
+    name: "Bitis Pink",
+    price: 683,
+    img: "image3.jpg",
+  },
+  {
+    id: 4,
+    name: "Addidas luxury",
+    price: 220,
+    img: "image3.jpg",
+  },
+];
